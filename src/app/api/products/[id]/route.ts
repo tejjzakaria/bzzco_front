d018082d82e261f703ff7aab41ca2644e7600f45
tejjzakaria <zakaria.tejjani@gmail.com>
2025-09-29 14:53:02 +0200

    fix: any types are replaced with specific types10

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -2,12 +2,14 @@ import { NextResponse, NextRequest } from 'next/server';
 import Product from '@/models/Product';
 import { connectToDatabase } from '@/lib/mongodb';
 
-export async function GET(request: NextRequest, context: { params: { id: string } }): Promise<Response> {
+export async function GET(
+  request: NextRequest,
+  context: { params: Promise<{ id: string }> }
+): Promise<Response> {
   await connectToDatabase();
-  const { params } = context;
+  const { id } = await context.params;
   try {
-    // Populate vendor (seller) info
-    const product = await Product.findById(params.id);
+    const product = await Product.findById(id);
     if (!product) {
       return NextResponse.json({ error: 'Product not found' }, { status: 404 });
     }
