0e1ce97394972be6796ac778215619906d2fce2c
tejjzakaria <zakaria.tejjani@gmail.com>
2025-10-07 20:51:01 +0200

    fix: ESlint fixes2

--- src/app/order-confirmation/page.tsx
+++ src/app/order-confirmation/page.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import Layout from '../../../components/Layout'
 import { NavBar } from '../../../components/NavBar'
 import Footer from '../../../components/Footer'
@@ -8,11 +8,12 @@ const page = () => {
   return (
     <Layout>
       <NavBar />
+      <Suspense>
+        <OrderConfirmation />
 
-      <OrderConfirmation />
-      
+      </Suspense>
       <Footer />
-      </Layout>
+    </Layout>
   )
 }
 
